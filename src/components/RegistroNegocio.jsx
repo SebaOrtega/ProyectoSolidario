8565c01916cf6ff2f496d3f86e525b1e7bfa1baf
SebastianOrtegaDV <sebastian.ortega.dv@gmail.com>
2021-06-10 21:47:35 -0300

    RegistroNegocioAvance

--- src/components/RegistroNegocio.jsx
+++ src/components/RegistroNegocio.jsx
@@ -3,16 +3,19 @@ import { useHistory } from "react-router";
 import "./styles/RegistroNegocio.css";
 const RegistroNegocio = () => {
   let history = useHistory();
-
   return (
     <div className="grid-container">
       <div className="derecha">
         <button onClick={() => history.goBack()}>Back</button>
 
         <div className="registronegocio-container">
-          <h1>Nuevo negocio</h1>
+          <h1>Registro de tu negocio</h1>
           <label htmlFor="negocio">Nombre del negocio</label>
           <input type="text" id="negocio" />
+
+
+          <label htmlFor="tipo-negocio">Tipo de negocio</label>
+          <input type="text" id="tipo-negocio" />
 
           <label htmlFor="email">Correo electrónico del negocio</label>
           <input type="text" id="email-negocio" />
